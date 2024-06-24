14e9bc49ea9193d2bdad6f17d41f4c2552eb05b4
Beecroft Tobi <tobilobabeecroft@gmail.com>
2024-06-24 20:10:35 +0100

    Third Commit

---
+++ Math Function.py
@@ -0,0 +1,12 @@
+from math import *
+num= floor(3.5)
+num2 = ceil(3.5)
+
+print(num)
+print(num2)
+
+area = pi * pow(3,2)
+print(f"{round(area,2)}")
+
+ch = input("Enter your name: ")[0]
+print(ch.capitalize())
