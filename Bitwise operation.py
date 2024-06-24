e427640082f86f21a6ff90cea2c25edaeefaf487
Beecroft Tobi <tobilobabeecroft@gmail.com>
2024-06-24 20:09:39 +0100

    Third Commit

---
+++ Bitwise operation.py
@@ -0,0 +1,20 @@
+num = 3
+print(~num)
+
+num2 = 7^5
+print(num2)
+
+num3 = 3&4
+print(num3)
+
+num4 = 3|4
+print(num4)
+
+print(7<<1)
+print(7>>1)
+
+expre = eval(input("Enter expression: "))
+print(expre)
+
+with open("Math Function.py","w") as w:
+    w.write("#")
